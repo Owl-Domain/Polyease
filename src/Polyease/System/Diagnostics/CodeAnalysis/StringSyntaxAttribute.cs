f05c2a70f11a9bd28b5874d82bfd15ea5c0ecb16
Nightowl <nightowl286@protonmail.com>
2024-03-19 01:41:42 +0000

    * Added stub information to the comments for the StringSyntaxAttribute.

--- src/Polyease/System/Diagnostics/CodeAnalysis/StringSyntaxAttribute.cs
+++ src/Polyease/System/Diagnostics/CodeAnalysis/StringSyntaxAttribute.cs
@@ -9,11 +9,19 @@ namespace System.Diagnostics.CodeAnalysis;
 #if !NET7_0_OR_GREATER
 
 /// <summary>Specifies the syntax used in a string.</summary>
+/// <remarks><b>
+///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+///   The actual implementation is only provided in .NET 7.0/later.
+///</b></remarks>
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
 public sealed class StringSyntaxAttribute : Attribute
 {
    /// <summary>Initializes the <see cref="StringSyntaxAttribute"/> with the identifier of the syntax used.</summary>
    /// <param name="syntax">The syntax identifier.</param>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public StringSyntaxAttribute(string syntax)
    {
       Syntax = syntax;
@@ -23,6 +31,10 @@ public sealed class StringSyntaxAttribute : Attribute
    /// <summary>Initializes the <see cref="StringSyntaxAttribute"/> with the identifier of the syntax used.</summary>
    /// <param name="syntax">The syntax identifier.</param>
    /// <param name="arguments">Optional arguments associated with the specific syntax employed.</param>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public StringSyntaxAttribute(string syntax, params object?[] arguments)
    {
       Syntax = syntax;
@@ -30,45 +42,101 @@ public sealed class StringSyntaxAttribute : Attribute
    }
 
    /// <summary>Gets the identifier of the syntax used.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public string Syntax { get; }
 
    /// <summary>Optional arguments associated with the specific syntax employed.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public object?[] Arguments { get; }
 
    /// <summary>The syntax identifier for strings containing composite formats for string formatting.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string CompositeFormat = nameof(CompositeFormat);
 
    /// <summary>The syntax identifier for strings containing date format specifiers.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string DateOnlyFormat = nameof(DateOnlyFormat);
 
    /// <summary>The syntax identifier for strings containing date and time format specifiers.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string DateTimeFormat = nameof(DateTimeFormat);
 
    /// <summary>The syntax identifier for strings containing <see cref="Enum"/> format specifiers.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string EnumFormat = nameof(EnumFormat);
 
    /// <summary>The syntax identifier for strings containing <see cref="Guid"/> format specifiers.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string GuidFormat = nameof(GuidFormat);
 
    /// <summary>The syntax identifier for strings containing JavaScript Object Notation (JSON).</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string Json = nameof(Json);
 
    /// <summary>The syntax identifier for strings containing numeric format specifiers.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string NumericFormat = nameof(NumericFormat);
 
    /// <summary>The syntax identifier for strings containing regular expressions.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string Regex = nameof(Regex);
 
    /// <summary>The syntax identifier for strings containing time format specifiers.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string TimeOnlyFormat = nameof(TimeOnlyFormat);
 
    /// <summary>The syntax identifier for strings containing <see cref="TimeSpan"/> format specifiers.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string TimeSpanFormat = nameof(TimeSpanFormat);
 
    /// <summary>The syntax identifier for strings containing URIs.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string Uri = nameof(Uri);
 
    /// <summary>The syntax identifier for strings containing XML.</summary>
+   /// <remarks><b>
+   ///   This is a stub provided by the <see href="https://github.com/Owl-Domain/Polyease">OwlDomain.Polyease</see> package.<br/>
+   ///   The actual implementation is only provided in .NET 7.0/later.
+   ///</b></remarks>
    public const string Xml = nameof(Xml);
 }
 
